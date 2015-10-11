bb423567c667d46baf2b432b343ca649ae69bf3a
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-11 12:18:53 -0400

    Create random generator for Character

--- RandomKitTests/RandomKitTests.swift
+++ RandomKitTests/RandomKitTests.swift
@@ -10,5 +10,16 @@ import XCTest
 import RandomKit
 
 class RandomKitTests: XCTestCase {
-    
+
+    let min = Character(UnicodeScalar(0))
+    let max = Character(UnicodeScalar(UInt8.max))
+
+    var interval: ClosedInterval<Character> { return min...max }
+
+    func testRandomCharacter() {
+        let r1 = Character.random(self.interval)
+        let r2 = Character.random(self.interval)
+        XCTAssertNotEqual(r1, r2, "Random characters are equal")
+    }
+
 }
