bd7c1216c937d098ca3202a0caf71e665d3a6fe5
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-10 22:01:19 -0400

    Create RandomKitTests

---
+++ RandomKitTests/RandomKitTests.swift
@@ -0,0 +1,14 @@
+//
+//  RandomKitTests.swift
+//  RandomKitTests
+//
+//  Created by Nikolai Vazquez on 10/10/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import XCTest
+import RandomKit
+
+class RandomKitTests: XCTestCase {
+    
+}
