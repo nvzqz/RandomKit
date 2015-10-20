7c1cf0bb0250df95c8127ea1f7db59b7c11794f4
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-20 00:05:06 -0400

    Conform CGVector to RandomType

---
+++ RandomKit/Foundation/CGVector+RandomKit.swift
@@ -0,0 +1,48 @@
+//
+//  CGVector+RandomKit.swift
+//  RandomKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import CoreGraphics
+
+extension CGVector {
+
+    /// Generates a random `CGVector`.
+    ///
+    /// - Returns: Random value within `0...100` for both `dx` and `dy`.
+    public static func random() -> CGVector {
+        return random(0...100, 0...100)
+    }
+
+    /// Generates a random `CGVector` inside of the closed intervals.
+    ///
+    /// - Parameters:
+    ///     - dxInterval: The interval within which `dx` will be generated.
+    ///     - dyInterval: The interval within which `dy` will be generated.
+    public static func random(dxInterval: ClosedInterval<CGFloat.NativeType>, _ dyInterval: ClosedInterval<CGFloat.NativeType>) -> CGVector {
+        return CGVector(dx: CGFloat.NativeType.random(dxInterval), dy: CGFloat.NativeType.random(dyInterval))
+    }
+
+}
