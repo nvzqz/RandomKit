0d23a5231b1c516e54ba77f7212543a4a7b6a797
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-19 23:05:47 -0400

    Conform CGSize to RandomType

---
+++ RandomKit/Foundation/CGSize+RandomKit.swift
@@ -0,0 +1,48 @@
+//
+//  CGSize+RandomKit.swift
+//  RandomKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import CoreGraphics
+
+extension CGSize: RandomType {
+
+    /// Generates a random `CGSize`.
+    ///
+    /// - Returns: Random value within `0...100` for both `width` and `height`.
+    public static func random() -> CGSize {
+        return random(0...100, 0...100)
+    }
+
+    /// Generates a random `CGSize` inside of the closed intervals.
+    ///
+    /// - Parameters:
+    ///     - widthInterval: The interval within which `width` will be generated.
+    ///     - heightInterval: The interval within which `height` will be generated.
+    public static func random(widthInterval: ClosedInterval<CGFloat.NativeType>, _ heightInterval: ClosedInterval<CGFloat.NativeType>) -> CGSize {
+        return CGSize(width: CGFloat.random(widthInterval), height: CGFloat.random(heightInterval))
+    }
+
+}
