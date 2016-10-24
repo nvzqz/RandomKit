fbf5a64390125410c969358450abbf8e8e9590f9
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-10-24 19:09:38 -0400

    Conform Decimal to RandomWithinClosedRange

---
+++ Sources/RandomKit/Extensions/Foundation/Decimal+RandomKit.swift
@@ -0,0 +1,43 @@
+//
+//  Decimal+RandomKit.swift
+//  RandomKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015-2016 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+extension Decimal: Random, RandomWithinClosedRange {
+
+    /// Generates a random value of `Self`.
+    public static func random() -> Decimal {
+        return random(within: 0...1)
+    }
+
+    /// Returns a random value of `Self` inside of the closed range.
+    public static func random(within closedRange: ClosedRange<Decimal>) -> Decimal {
+        let multiplier = closedRange.upperBound - closedRange.lowerBound
+        return closedRange.lowerBound + multiplier * (Decimal(UInt32.random()) / Decimal(UInt32.max))
+    }
+
+}
