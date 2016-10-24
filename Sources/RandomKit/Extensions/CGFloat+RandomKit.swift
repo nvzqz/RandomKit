930889425af12c5b6443ec47e0a997d5948c3941
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-10-24 16:57:36 -0400

    Remove duplicate random methods from CGFloat
    Already inherited from FloatingPoint

--- Sources/RandomKit/Extensions/CGFloat+RandomKit.swift
+++ Sources/RandomKit/Extensions/CGFloat+RandomKit.swift
@@ -29,19 +29,6 @@ import CoreGraphics
 
 extension CGFloat: Random, RandomWithinClosedRange {
 
-    /// Generates a random `CGFloat`.
-    ///
-    /// - Returns: Random value within `0.0...1.0`.
-    public static func random() -> CGFloat {
-        return random(within: 0.0...1.0)
-    }
-
-    /// Returns a random value of `Self` inside of the closed range.
-    public static func random(within closedRange: ClosedRange<CGFloat>) -> CGFloat {
-        return random(within: .init(uncheckedBounds: (closedRange.lowerBound.native,
-                                                      closedRange.upperBound.native)))
-    }
-
     /// Generates a random `CGFloat` within the closed range.
     ///
     /// - parameter closedRange: The range within which the number will be generated.
