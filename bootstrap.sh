53d6c882dd6ae44359af00a022fe441b348bfa53
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-10-30 15:05:18 -0400

    Use ZSH for Vagrant shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,14 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y git clang libicu-dev
-
 export SWIFT_VERSION="swift-3.0"
 
-eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/9f442512a46d7a2af7b850d65a7e9bd31edfb09b/swiftenv-install.sh)"
+sudo apt-get update
+sudo apt-get install -y git zsh clang libicu-dev
 
-echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> .profile
-echo 'export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"' >> .profile
+sudo chsh -s /bin/zsh vagrant
+
+echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.zshrc
+echo 'export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"' >> ~/.zshrc
+
+eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/9f442512a46d7a2af7b850d65a7e9bd31edfb09b/swiftenv-install.sh)"
