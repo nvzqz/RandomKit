2eb1713d5292c4b594ea9862919a1a57bf30e6ac
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-10-30 15:39:54 -0400

    Add RandomWithinRange functions to Strideable
    Conformed UTF16Index, UnsafePointer, UnsafeMutablePointer,
    UnsafeRawPointer, and UnsafeMutableRawPointer to RandomWithinRange and
    RandomWithinClosedRange.

---
+++ Sources/RandomKit/Extensions/Swift/Strideable+RandomKit.swift
@@ -0,0 +1,64 @@
+//
+//  Strideable+RandomKit.swift
+//  RandomKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015-2016 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+extension Strideable where Self: RandomWithinRange, Stride: RandomToValue {
+
+    /// Returns an optional random value of `Self` inside of the range using `randomGenerator`.
+    public static func random(within range: Range<Self>, using randomGenerator: RandomGenerator) -> Self? {
+        guard !range.isEmpty else {
+            return nil
+        }
+        let distance = range.lowerBound.distance(to: range.upperBound)
+        return range.lowerBound.advanced(by: .random(to: distance, using: randomGenerator))
+    }
+
+}
+
+extension Strideable where Self: RandomWithinClosedRange, Stride: RandomThroughValue {
+
+    /// Returns a random value of `Self` inside of the closed range using `randomGenerator`.
+    public static func random(within closedRange: ClosedRange<Self>, using randomGenerator: RandomGenerator) -> Self {
+        let distance = closedRange.lowerBound.distance(to: closedRange.upperBound)
+        return closedRange.lowerBound.advanced(by: .random(through: distance, using: randomGenerator))
+    }
+
+}
+
+extension String.UTF16Index: RandomWithinRange, RandomWithinClosedRange {
+}
+
+extension UnsafePointer: RandomWithinRange, RandomWithinClosedRange {
+}
+
+extension UnsafeMutablePointer: RandomWithinRange, RandomWithinClosedRange {
+}
+
+extension UnsafeRawPointer: RandomWithinRange, RandomWithinClosedRange {
+}
+
+extension UnsafeMutableRawPointer: RandomWithinRange, RandomWithinClosedRange {
+}
