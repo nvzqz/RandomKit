8dc67b0f2a5dab90fa5755d7a11ecfddb0a7bf3f
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-01-21 15:34:36 -0500

    Make Array init(randomCount:) faster

--- Sources/RandomKit/Extensions/Swift/Array+RandomKit.swift
+++ Sources/RandomKit/Extensions/Swift/Array+RandomKit.swift
@@ -31,7 +31,7 @@ extension Array where Element: Random {
     ///
     /// Although safety is not guaranteed, `init(unsafeRandomCount:)` is *significantly* faster than this.
     public init<R: RandomGenerator>(randomCount: Int, using randomGenerator: inout R) {
-        self = (0 ..< randomCount).map { _ in Element.random(using: &randomGenerator) }
+        self.init(Element.randoms(limitedBy: randomCount, using: &randomGenerator))
     }
 
 }
@@ -53,13 +53,7 @@ extension Array where Element: RandomWithinRange {
 
     /// Construct an Array of random elements from within the range.
     public init<R: RandomGenerator>(randomCount: Int, within range: Range<Element>, using randomGenerator: inout R) {
-        if range.isEmpty || randomCount <= 0 {
-            self = []
-        } else {
-            self = CountableRange(uncheckedBounds: (0, randomCount)).map { _ in
-                Element.random(within: range, using: &randomGenerator).unsafelyUnwrapped
-            }
-        }
+        self.init(Element.randoms(limitedBy: randomCount, within: range, using: &randomGenerator))
     }
 
 }
@@ -68,13 +62,7 @@ extension Array where Element: RandomWithinClosedRange {
 
     /// Construct an Array of random elements from within the closed range.
     public init<R: RandomGenerator>(randomCount: Int, within closedRange: ClosedRange<Element>, using randomGenerator: inout R) {
-        if randomCount <= 0 {
-            self = []
-        } else {
-            self = CountableRange(uncheckedBounds: (0, randomCount)).map { _ in
-                Element.random(within: closedRange, using: &randomGenerator)
-            }
-        }
+        self.init(Element.randoms(limitedBy: randomCount, within: closedRange, using: &randomGenerator))
     }
 
 }
