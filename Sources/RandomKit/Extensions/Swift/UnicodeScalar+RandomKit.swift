nvzqz/RandomKit

commit dd5d42c16c1db3ec55a5b9426b16672c7c931871

Use cast instead of unwrap in UnicodeScalar

Sources/RandomKit/Extensions/Swift/UnicodeScalar+RandomKit.swift
@@ -67,13 +67,14 @@ extension UnicodeScalar: Random, RandomWithinRange, RandomWithinClosedRange {
             let newRange = ClosedRange(uncheckedBounds: (lower, upper - diff))
             let random = UInt32.random(within: newRange, using: &randomGenerator)
             if random._isLowerRange {
-                return UnicodeScalar(random).unsafelyUnwrapped
+                return unsafeBitCast(random, to: UnicodeScalar.self)
             } else {
-                return UnicodeScalar(random + diff).unsafelyUnwrapped
+                return unsafeBitCast(random, to: UnicodeScalar.self)
             }
         } else {
             let newRange = ClosedRange(uncheckedBounds: (lower, upper))
-            return UnicodeScalar(.random(within: newRange, using: &randomGenerator)).unsafelyUnwrapped
+            let random = UInt32.random(within: newRange, using: &randomGenerator)
+            return unsafeBitCast(random, to: UnicodeScalar.self)
         }
     }
 

commit 0477a8e68d195a84b32cacb3f124eaa8c2b52987

Fix upper range correction for UnicodeScalar
Commit dd5d42c16c1db3ec55a5b9426b16672c7c931871 accidentally removed the
range correction for UnicodeScalar `random(within:using:)` for closed
ranges.

Sources/RandomKit/Extensions/Swift/UnicodeScalar+RandomKit.swift
@@ -48,7 +48,7 @@ extension UnicodeScalar: Random, RandomWithinRange, RandomWithinClosedRange {
             if random._isLowerRange {
                 return unsafeBitCast(random, to: UnicodeScalar.self)
             } else {
-                return unsafeBitCast(random + diff, to: UnicodeScalar.self)
+                return unsafeBitCast(random &+ diff, to: UnicodeScalar.self)
             }
         } else {
             let newRange = Range(uncheckedBounds: (lower, upper))
@@ -69,7 +69,7 @@ extension UnicodeScalar: Random, RandomWithinRange, RandomWithinClosedRange {
             if random._isLowerRange {
                 return unsafeBitCast(random, to: UnicodeScalar.self)
             } else {
-                return unsafeBitCast(random, to: UnicodeScalar.self)
+                return unsafeBitCast(random &+ diff, to: UnicodeScalar.self)
             }
         } else {
             let newRange = ClosedRange(uncheckedBounds: (lower, upper))