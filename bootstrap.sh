93dfa84b0634f976eee2cf04e174342ba158e40f
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-03-06 20:27:36 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,24 @@
 #
 # A provision script for Vagrant to setup a Swift-ready development environment.
 # Author: Nikolai Vazquez (https://github.com/nvzqz)
-# Platform: Ubuntu 14.04  (ubuntu/trusty64)
 
 set -e
-
-export SWIFT_VERSION="swift-3.0"
 
 sudo apt-get update
 sudo apt-get install -y git zsh clang libicu-dev
 
 sudo chsh -s /bin/zsh vagrant
 
-echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.zshrc
-echo 'export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"' >> ~/.zshrc
+SWIFT_VERSION="swift-3.0.2"
+SWIFTENV_ROOT="$HOME/.swiftenv"
 
-eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/9f442512a46d7a2af7b850d65a7e9bd31edfb09b/swiftenv-install.sh)"
+if [ ! -d "$SWIFTENV_ROOT" ]; then
+    git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
+    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.zshrc
+    echo 'export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"' >> ~/.zshrc
+fi
+
+source ~/.zshrc
+
+echo "Installing $SWIFT_VERSION..."
+swiftenv install "$SWIFT_VERSION" > /dev/null 2>&1
