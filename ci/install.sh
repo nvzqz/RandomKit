40c4fce9c7ab23046d9595b4a0483479d3c1505c
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-06-27 16:20:01 -0400

    Revert "Cache swiftenv root in travis"
    This reverts commit 510238396be33ee78c547c7628414abf07147246.

--- ci/install.sh
+++ ci/install.sh
@@ -3,14 +3,10 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    if [[ ! -d "$SWIFTENV_ROOT" ]]; then
-        git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
-    fi
+    git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
 
     for version in $SWIFT_VERSIONS; do
-        if ! swiftenv global $version;
-            then swiftenv install "$version"
-        fi
+        swiftenv install "$version"
     done
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     gem install xcpretty -N
