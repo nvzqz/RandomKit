189ede8bff3e6d661d218e4243901a327d66b2aa
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-06-27 11:43:21 -0400

    Make separate script for travis

---
+++ ci/travis.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    swift test
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    xcodebuild -version
+
+    xcodebuild \
+        -project $FRAMEWORK_NAME.xcodeproj \
+        -scheme "$FRAMEWORK_NAME macOS" \
+        ONLY_ACTIVE_ARCH=YES \
+        test | xcpretty
+
+    xcodebuild \
+        -project $FRAMEWORK_NAME.xcodeproj \
+        -scheme "$FRAMEWORK_NAME iOS" \
+        -sdk iphonesimulator \
+        -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" \
+        ONLY_ACTIVE_ARCH=NO \
+        test | xcpretty
+
+    xcodebuild \
+        -project $FRAMEWORK_NAME.xcodeproj \
+        -scheme "$FRAMEWORK_NAME tvOS" \
+        -sdk appletvsimulator \
+        -destination "platform=tvOS Simulator,name=Apple TV 1080p" \
+        ONLY_ACTIVE_ARCH=NO \
+        test | xcpretty
+
+    pod lib lint --quick
+fi
