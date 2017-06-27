0f5468555afab0e8b30c150235458b4e5a551807
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-06-27 12:56:03 -0400

    Set SWIFTENV_ROOT and PATH in travis config

--- ci/install.sh
+++ ci/install.sh
@@ -3,13 +3,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export SWIFTENV_ROOT="$HOME/.swiftenv"
     git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
-    export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
-
-    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bash_profile
-    echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(swiftenv init -)"' >> ~/.bash_profile
 
     for version in $SWIFT_VERSIONS; do
         swiftenv install "$version"
