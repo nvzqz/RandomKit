1731c1a086c51efbd14cd6f5a36d503acd27dbbd
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-03 15:37:43 -0400

    [Travis] Install cocoapods with xcpretty

--- ci/install.sh
+++ ci/install.sh
@@ -6,5 +6,5 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
     swiftenv install "$SWIFT_VERSION"
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    gem install xcpretty -N
+    gem install cocoapods xcpretty --no-rdoc --no-ri --no-document
 fi
