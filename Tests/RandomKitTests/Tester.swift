bbc69f297a28618f15474b221f516de079536200
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-02 20:35:49 -0400

    Use pointer in tests to work with Swift < 3.1

--- Tests/RandomKitTests/Tester.swift
+++ Tests/RandomKitTests/Tester.swift
@@ -33,7 +33,7 @@ let defaultTestCount: UInt = 100_000
 public protocol Tester {
     associatedtype Gen: RandomGenerator
 
-    func withTestGenerator(_ body: (inout Gen) throws -> ()) rethrows
+    func withTestGenerator(_ body: (UnsafeMutablePointer<Gen>) throws -> ()) rethrows
 
     func testRandomInt()
     func testRandomOpen()
@@ -61,7 +61,7 @@ extension Tester {
             for max in stride(from: 1, through: Int.max - 128, by: Int.max / 128) {
                 let range = (-max)...max
                 for _ in 0 ..< count / 128 {
-                    let num = Int.random(in: range, using: &gen)
+                    let num = Int.random(in: range, using: &gen.pointee)
                     XCTAssertTrue(range ~= num, "\(num) is out of bounds of \(range).")
                 }
             }
@@ -75,8 +75,8 @@ extension Tester {
                 XCTAssertLessThan(value, 1.0)
             }
             for _ in 0 ..< count {
-                test(with: gen.randomOpen32())
-                test(with: gen.randomOpen64())
+                test(with: gen.pointee.randomOpen32())
+                test(with: gen.pointee.randomOpen64())
             }
         }
     }
@@ -88,8 +88,8 @@ extension Tester {
                 XCTAssertLessThanOrEqual(value, 1.0)
             }
             for _ in 0 ..< count {
-                test(with: gen.randomClosed32())
-                test(with: gen.randomClosed64())
+                test(with: gen.pointee.randomClosed32())
+                test(with: gen.pointee.randomClosed64())
             }
         }
     }
@@ -99,7 +99,7 @@ extension Tester {
             let min = -1.5
             let max =  0.5
             for _ in 0 ..< count {
-                let r = Double.random(in: min...max, using: &gen)
+                let r = Double.random(in: min...max, using: &gen.pointee)
                 XCTAssertTrue(r >= min && r <= max, "Random `Double` is out of bounds.")
             }
         }
@@ -110,7 +110,7 @@ extension Tester {
             let min: Float = -1.5
             let max: Float =  0.5
             for _ in 0 ..< count {
-                let r = Float.random(in: min...max, using: &gen)
+                let r = Float.random(in: min...max, using: &gen.pointee)
                 XCTAssertTrue(r >= min && r <= max, "Random `Float` is out of bounds.")
             }
         }
@@ -119,7 +119,7 @@ extension Tester {
     func testRandomBool(count: UInt) {
         withTestGenerator { gen in
             let falseCount = (0 ..< count).reduce(0) { count, _ in
-                Bool.random(using: &gen) ? count : count + 1
+                Bool.random(using: &gen.pointee) ? count : count + 1
             }
             let difference = 0.5
             let percentRange = (50 - difference)...(50 + difference)
@@ -131,46 +131,46 @@ extension Tester {
     func testRandomArraySliceImpl() {
         withTestGenerator { gen in
             let count = 10
-            let array: [Int] = Array(randomCount: count, using: &gen)
+            let array: [Int] = Array(randomCount: count, using: &gen.pointee)
             let sliceCount = count / 2
 
-            var result = array.randomSlice(count: sliceCount, using: &gen)
+            var result = array.randomSlice(count: sliceCount, using: &gen.pointee)
             XCTAssertEqual(result.count, sliceCount)
             let simpleSlice = Array(array[0..<sliceCount])
             XCTAssertNotEqual(result, simpleSlice)
 
-            result = array.randomSlice(count: count, using: &gen) // all
+            result = array.randomSlice(count: count, using: &gen.pointee) // all
             XCTAssertEqual(result.count, count)
 
-            result = array.randomSlice(count: count * 2, using: &gen) // too much
+            result = array.randomSlice(count: count * 2, using: &gen.pointee) // too much
             XCTAssertEqual(result.count, count)
 
-            result = array.randomSlice(count: 0, using: &gen) // nothing
+            result = array.randomSlice(count: 0, using: &gen.pointee) // nothing
             XCTAssertEqual(result.count, 0)
 
             let weightsArray: [[Double]] = [
-                Array(randomCount: count, using: &gen),
-                Array(randomCount: count, in: 0...100, using: &gen)
+                Array(randomCount: count, using: &gen.pointee),
+                Array(randomCount: count, in: 0...100, using: &gen.pointee)
             ]
 
             for weights in weightsArray {
-                result = array.randomSlice(count: count, weights: weights, using: &gen) // all
+                result = array.randomSlice(count: count, weights: weights, using: &gen.pointee) // all
                 XCTAssertEqual(result.count, count)
 
-                result = array.randomSlice(count: sliceCount, weights: weights, using: &gen)
+                result = array.randomSlice(count: sliceCount, weights: weights, using: &gen.pointee)
                 XCTAssertEqual(result.count, sliceCount)
 
                 result = array.randomSlice(count: count * 2,
                                            weights: weights,
-                                           using: &gen) // too much
+                                           using: &gen.pointee) // too much
                 XCTAssertEqual(result.count, count)
 
                 result = array.randomSlice(count: count * 2,
-                                           weights: Array(randomCount: count / 2, using: &gen),
-                                           using: &gen) // partial weights
+                                           weights: Array(randomCount: count / 2, using: &gen.pointee),
+                                           using: &gen.pointee) // partial weights
                 XCTAssertEqual(result.count, count)
 
-                result = array.randomSlice(count: 0, weights: weights, using: &gen) // nothing
+                result = array.randomSlice(count: 0, weights: weights, using: &gen.pointee) // nothing
                 XCTAssertEqual(result.count, 0)
             }
         }
@@ -179,7 +179,7 @@ extension Tester {
 }
 
 extension Tester where Gen: SeedableFromRandomGenerator {
-    func withTestGenerator(_ body: (inout Gen) throws -> ()) rethrows {
-        try Gen.withThreadLocal(body)
+    func withTestGenerator(_ body: (UnsafeMutablePointer<Gen>) throws -> ()) rethrows {
+        try body(Gen.threadLocal)
     }
 }
